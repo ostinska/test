3bc9221256f15cf5739df0923efd6b7aeba16f9e
ostinska <162920321+ostinska@users.noreply.github.com>
2025-06-02 00:19:48 +0300

    Add files via upload

---
+++ test/js/main.js
@@ -0,0 +1,13 @@
+
+const partnersSlider = new Swiper('.ticker', {
+	speed: 8000, // Швидкість прокрутки
+	spaceBetween: 80, // Відступ між слайдами
+	loop: true, // Нескінченний цикл
+	slidesPerView: 'auto',
+	allowTouchMove: false,
+	autoplay: {
+		delay: 0,
+		disableOnInteraction: false
+	},
+});
+
